c51deb9dfe643ae163da3ef19010b47f17238609
bellaratmelia <bella.ratmelia@gmail.com>
2017-07-05 10:48:17 +0800

    added codes for news iframe

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 var $overlay = $('<div id="overlay"></div>');
 var $vid = $('<video src="" autoplay controls></video>');
 var video;
+var iframe_link;
 
 // Append the components to overlay, then append it to html body
 $overlay.append($vid);
@@ -28,4 +29,15 @@ $(".video-item a").click(function(event) {
 	
 	// Show overlay
 	$overlay.fadeIn();
+});
+
+//news iframe stuff
+$(".news-headline").click(function(event) {
+	event.preventDefault();
+	
+	// grab the link from href
+	iframe_link = $(this).attr("href");
+	
+	//update iframe
+	$("#news-iframe").attr("src", iframe_link);
 });
