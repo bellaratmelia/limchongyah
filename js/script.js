40d45fd8ed12e3fe700320e327e49887fad088e6
vinnyfav <30652386+vinnyfav@users.noreply.github.com>
2017-09-21 16:10:47 +0800

    refresh the link to cvs (for the aug15 commit)
    I forgot to edit the js for the Aug15 commit. This should load the newest csv with the changes made during that Aug15 commit that was done to fix the newspaperSG broken links.

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ $(".video-item a").click(function(event) {
 // CSV handling 
 $.ajax({
 	// alt URL: https://rawgit.com/bellaratmelia/limchongyah/master/lcy_news.csv
-    url: "https://cdn.rawgit.com/bellaratmelia/limchongyah/4caea731/lcy_news.csv",
+    url: "https://cdn.rawgit.com/bellaratmelia/limchongyah/954f8d77/lcy_news.csv",
     async: true,
     success: function (csvd) {
         items = $.csv.toObjects(csvd);
